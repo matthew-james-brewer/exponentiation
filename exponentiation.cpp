191ef7f91ff0d9892c5ec87df66220425f2fc9d7
Matthew <105758744+matthew-james-brewer@users.noreply.github.com>
2023-06-17 09:03:54 -0500

    Update MyMathLibrary.cpp

--- exponentiation.cpp
+++ exponentiation.cpp
@@ -1,235 +1,239 @@
 #include <cmath>
 #include <iostream>
 
-// Class for middle of eint **eint = eint *(*eint) = eint *zint
-class zint
+namespace MyMathLibrary
 {
-	private:
-		int number;
-
-	public:
-		zint(int num): number(num) {}
-
-	operator int() const
+	// Class for middle of eint **eint = eint *(*eint) = eint *zint
+	class zint
 	{
-		return number;
-	}
-};
+		private:
+			int number;
 
-class eint
-{
-	private:
-		int number;
+		public:
+			zint(int num): number(num) {}
 
-	public:
-		eint(int num): number(num) {}
+		operator int() const
+		{
+			return number;
+		}
+	};
 
-	eint(const eint &ext): number(ext.number) {}
-
-	// Overload the *eint operator to create an zint for exponentiation
-	zint operator*()
+	class eint
 	{
-		return zint(number);
-	}
+		private:
+			int number;
 
-	// Overload the multiplication operator to handle eint *zint for exponentiation
-	eint operator*(zint other)
-	{
-		return eint(std::pow(double(number), double(int(other))));
-	}
+		public:
+			eint(int num): number(num) {}
 
-	operator int() const
-	{
-		return number;
-	}
+		eint(const eint &ext): number(ext.number) {}
 
-	// Handle bool operators
+		// Overload the *eint operator to create an zint for exponentiation
+		zint operator*()
+		{
+			return zint(number);
+		}
 
-	bool operator==(eint e)
-	{
-		return (number == int(e));
-	}
+		// Overload the multiplication operator to handle eint *zint for exponentiation
+		eint operator*(zint other)
+		{
+			return eint(std::pow(double(number), double(int(other))));
+		}
 
-	bool operator!=(eint e)
-	{
-		return (number != int(e));
-	}
+		operator int() const
+		{
+			return number;
+		}
 
-	bool operator < (eint e)
-	{
-		return (number < int(e));
-	}
+		// Handle bool operators
 
-	bool operator>(eint e)
-	{
-		return (number > int(e));
-	}
+		bool operator==(eint e)
+		{
+			return (number == int(e));
+		}
 
-	bool operator<=(eint e)
-	{
-		return ((number < int(e)) || (number == int(e)));
-	}
+		bool operator!=(eint e)
+		{
+			return (number != int(e));
+		}
 
-	bool operator>=(eint e)
-	{
-		return ((number > int(e)) || (number == int(e)));
-	}
+		bool operator < (eint e)
+		{
+			return (number < int(e));
+		}
 
-	// Handle[operator] and assign (and ++ / --)
+		bool operator>(eint e)
+		{
+			return (number > int(e));
+		}
 
-	eint &operator+=(eint e)
-	{
-		number += int(e);
-		return * this;
-	}
+		bool operator<=(eint e)
+		{
+			return ((number < int(e)) || (number == int(e)));
+		}
 
-	eint &operator++()
-	{
-		number++;
-		return * this;
-	}
+		bool operator>=(eint e)
+		{
+			return ((number > int(e)) || (number == int(e)));
+		}
 
-	eint operator++(int dummy)
-	{
-		eint preserve = *this;	// Preseve old number
-		number++;
-		return preserve;
-	}
+		// Handle[operator] and assign (and ++ / --)
 
-	eint &operator-=(eint e)
-	{
-		number -= int(e);
-		return * this;
-	}
+		eint &operator+=(eint e)
+		{
+			number += int(e);
+			return * this;
+		}
 
-	eint &operator--()
-	{
-		number--;
-		return * this;
-	}
+		eint &operator++()
+		{
+			number++;
+			return * this;
+		}
 
-	eint operator--(int dummy)
-	{
-		eint preserve = *this;	// Preserve old number
-		number--;
-		return preserve;
-	}
+		eint operator++(int dummy)
+		{
+			eint preserve = *this;	// Preseve old number
+			number++;
+			return preserve;
+		}
 
-	eint &operator*=(eint e)
-	{
-		number *= int(e);
-		return * this;
-	}
+		eint &operator-=(eint e)
+		{
+			number -= int(e);
+			return * this;
+		}
 
-	eint &operator/=(eint e)
-	{
-		number /= int(e);
-		return * this;
-	}
+		eint &operator--()
+		{
+			number--;
+			return * this;
+		}
 
-	eint &operator%=(eint e)
-	{
-		number %= int(e);
-		return * this;
-	}
+		eint operator--(int dummy)
+		{
+			eint preserve = *this;	// Preserve old number
+			number--;
+			return preserve;
+		}
 
-	// Extra operators
-	eint &operator^=(eint e)
-	{
-		number ^= int(e);
-		return * this;
-	}
+		eint &operator*=(eint e)
+		{
+			number *= int(e);
+			return * this;
+		}
 
-	eint &operator&=(eint e)
-	{
-		number &= int(e);
-		return * this;
-	}
+		eint &operator/=(eint e)
+		{
+			number /= int(e);
+			return * this;
+		}
 
-	eint &operator|=(eint e)
-	{
-		number |= int(e);
-		return * this;
-	}
+		eint &operator%=(eint e)
+		{
+			number %= int(e);
+			return * this;
+		}
 
-	eint &operator>>=(eint e)
-	{
-		number >>= int(e);
-		return * this;
-	}
+		// Extra operators
+		eint &operator^=(eint e)
+		{
+			number ^= int(e);
+			return * this;
+		}
 
-	eint &operator<<=(eint e)
-	{
-		number <<= int(e);
-		return * this;
-	}
+		eint &operator&=(eint e)
+		{
+			number &= int(e);
+			return * this;
+		}
 
-	// Define +, -, *, /, % operators
+		eint &operator|=(eint e)
+		{
+			number |= int(e);
+			return * this;
+		}
 
-	eint operator+(eint e)
-	{
-		return eint(number + int(e));
-	}
+		eint &operator>>=(eint e)
+		{
+			number >>= int(e);
+			return * this;
+		}
 
-	eint operator-(eint e)
-	{
-		return eint(number - int(e));
-	}
+		eint &operator<<=(eint e)
+		{
+			number <<= int(e);
+			return * this;
+		}
 
-	eint operator*(eint e)
-	{
-		return eint(number* int(e));
-	}
+		// Define +, -, *, /, % operators
 
-	eint operator/(eint e)
-	{
-		return eint(number / int(e));
-	}
+		eint operator+(eint e)
+		{
+			return eint(number + int(e));
+		}
 
-	eint operator%(eint e)
-	{
-		return eint(number % int(e));
-	}
+		eint operator-(eint e)
+		{
+			return eint(number - int(e));
+		}
 
-	// Extra operators
-	eint operator^(eint e)
-	{
-		return eint(number ^ int(e));
-	}
+		eint operator*(eint e)
+		{
+			return eint(number* int(e));
+		}
 
-	eint operator&(eint e)
-	{
-		return eint(number &int(e));
-	}
+		eint operator/(eint e)
+		{
+			return eint(number / int(e));
+		}
 
-	eint operator|(eint e)
-	{
-		return eint(number | int(e));
-	}
+		eint operator%(eint e)
+		{
+			return eint(number % int(e));
+		}
 
-	eint operator>>(eint e)
-	{
-		return eint(number >> int(e));
-	}
+		// Extra operators
+		eint operator^(eint e)
+		{
+			return eint(number ^ int(e));
+		}
 
-	eint operator<<(eint e)
-	{
-		return eint(number << int(e));
-	}
+		eint operator&(eint e)
+		{
+			return eint(number &int(e));
+		}
 
-	// Define <<and>> operators
+		eint operator|(eint e)
+		{
+			return eint(number | int(e));
+		}
 
-	friend std::ostream &operator<<(std::ostream &os, const eint &e)
-	{
-		os << int(e);
-		return os;
-	}
+		eint operator>>(eint e)
+		{
+			return eint(number >> int(e));
+		}
 
-	friend std::istream &operator>>(std::istream &is, eint &e)
-	{
-		int value;
-		is >> value;
-		e = eint(value);
-		return is;
-	}
-};
+		eint operator<<(eint e)
+		{
+			return eint(number << int(e));
+		}
+
+		// Define <<and>> operators
+
+		friend std::ostream &operator<<(std::ostream &os, const eint &e)
+		{
+			os << int(e);
+			return os;
+		}
+
+		friend std::istream &operator>>(std::istream &is, eint &e)
+		{
+			int value;
+			is >> value;
+			e = eint(value);
+			return is;
+		}
+	};
+
+}
